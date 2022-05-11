be1edcc5d4669b6f138566305ecc6690fcafb7ef
Taran Roth <taranr5080@gmail.com>
2022-05-11 06:25:51 -0400

    Eliminate complex numbers

--- src/object.py
+++ src/object.py
@@ -16,28 +16,43 @@ class Object:
         self.data = data
         self.populate_forces()
 
+    def get_rad(self, vert, hori):
+        if vert == 0 and hori == 0:  
+            return 0
+        elif vert == 0 and hori > 0:
+            return 0
+        elif vert == 0 and hori < 0:
+            return math.pi()
+        elif hori == 0 and vert > 0:
+            return math.pi() / 2
+        elif hori == 0 and vert < 0:
+            return math.pi() * 1.5
+        return math.atan(vert / hori)
+
     def get_nforce(self, time):
         vert_nforce = 0
         hori_nforce = 0
         for force in self.forces:
             if time >= force.start and time <= force.end:
-                vert_nforce += sin(force.ang) * force.mag
-                hori_nforce += cos(force.ang) * force.mag
+                vert_nforce += sin(force.ang).real * force.mag
+                hori_nforce += cos(force.ang).real * force.mag
         nforce_mag = math.sqrt(vert_nforce ** 2 + hori_nforce ** 2)
-        nforce_ang = math.degrees(math.atan(vert_nforce/hori_nforce))
+        nforce_ang = self.get_rad(vert_nforce, hori_nforce)
         return [nforce_mag, nforce_ang]
 
     def tick(self, time, current_time):
         nforce = self.get_nforce(current_time)
-        init_vert_vel = sin(self.data["rd"]) * self.data["m/s"]
-        vert_vel = (sin(nforce[1]) * nforce[0] * time) / self.data["kg"] + init_vert_vel
+        init_vert_vel = sin(self.data["rd"]).real * self.data["m/s"]
+        vert_vel = (sin(nforce[1]).real * nforce[0] * time) / self.data["kg"] + init_vert_vel
         self.data["y"] += (vert_vel + init_vert_vel) / 2 * time
-        init_hori_vel = cos(self.data["rd"]) * self.data["m/s"]
-        hori_vel = (cos(nforce[1]) * nforce[0] * time) / self.data["kg"] + init_hori_vel
+        init_hori_vel = cos(self.data["rd"]).real * self.data["m/s"]
+        hori_vel = (cos(nforce[1]).real * nforce[0] * time) / self.data["kg"] + init_hori_vel
         self.data["x"] += (hori_vel + init_hori_vel) / 2 * time
+        self.data["m/s"] = math.sqrt(vert_vel ** 2 + hori_vel ** 2)
+        self.data["rad"] = self.get_rad(vert_vel, hori_vel)
 
     def simulate(self):
-        times_to_tick = 100
+        times_to_tick = 10000
         current_time = 0
         for i in range(times_to_tick):
             time_to_tick = int(self.data["sec"]) / times_to_tick
